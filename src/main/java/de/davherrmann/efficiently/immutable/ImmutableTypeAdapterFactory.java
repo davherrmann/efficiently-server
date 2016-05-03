7a6020f45bc7401ae78fe66a8ee245ee00028f49
David Herrmann <davherrmann@googlemail.com>
2016-05-03 18:02:37 +0200

    simplify TypeAdapterFactory

--- src/main/java/de/davherrmann/efficiently/immutable/ImmutableTypeAdapterFactory.java
+++ src/main/java/de/davherrmann/efficiently/immutable/ImmutableTypeAdapterFactory.java
@@ -15,23 +15,6 @@ public class ImmutableTypeAdapterFactory implements TypeAdapterFactory
     @Override
     public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type)
     {
-        if (Immutable.class.isAssignableFrom(type.getRawType()))
-        {
-            return new TypeAdapter<T>()
-            {
-                @Override
-                public void write(JsonWriter out, T value) throws IOException
-                {
-                    gson.getAdapter(Map.class).write(out, ((Immutable) value).values());
-                }
-                @Override
-                public T read(JsonReader in) throws IOException
-                {
-                    return null;
-                }
-            };
-        }
-
         if (Immutable.ImmutableNode.class.isAssignableFrom(type.getRawType()))
         {
             return new TypeAdapter<T>()
