b82c65a71645b3ed0e3235b25df294d18b25aff2
David Herrmann <davherrmann@googlemail.com>
2016-05-10 18:20:11 +0200

    add todo

--- src/main/java/de/davherrmann/efficiently/server/Action.java
+++ src/main/java/de/davherrmann/efficiently/server/Action.java
@@ -5,5 +5,6 @@ public interface Action<T>
     String type();
 
     // TODO allow typed Action? with immutable data? returning from asynchronous service call?
+    // TODO just use a Map<String, String>, extended Actions should use custom data-providing methods
     T meta();
 }
